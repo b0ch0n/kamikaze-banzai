400a017806486125f5ffa2e53ff408239a95f372
b0ch0n <guardia.exe@gmail.com>
2016-01-02 22:38:21 -0300

    Cambio de rendimiento al seguir el camino con el avion
    Se modificó _process por _fixed_process para ver si funciona de igual
    manera en máquinas de distinto tipo de características de hardware, ej
    mi PC(AMD) y Notebook(Intel).

--- Scripts/follower.gd
+++ Scripts/follower.gd
@@ -1,20 +1,24 @@
 
 extends PathFollow2D
 
-# member variables here, example:
-# var a=2
-# var b="textvar"
-var count_offset=0.0
+# member variables here
+var count_offset = 0.0
+
+# El siguiente valor en Intel(0.25) produce el movimiento rapido
+# mientras que en AMD tengo que aumentar 20 veces su valor
+# para que empiece a moverse similar al movimiento en Intel
+var delta_offset = 5 #0.25 # para Intel
+#var delta_offset = 0.25*20 # para AMD
+
+
 func _ready():
 	# Initialization here
-	set_process(true)
+	#set_process(true)
+	set_fixed_process(true)
 
 
-func _process(delta):
-#	print(delta)
-	count_offset+=0.25
+#func _process(delta):
+func _fixed_process(delta):
+	count_offset+=delta_offset
 	
 	set_offset(count_offset)
-	
-#	if Input
-
