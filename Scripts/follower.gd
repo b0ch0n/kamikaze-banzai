8a09e4c50f7a44b148f9aa35216f1ec19ce008f2
b0ch0n <guardia.exe@gmail.com>
2016-01-04 03:59:31 -0300

    Test tortoisesvn con modificacion de set_process

--- Scripts/follower.gd
+++ Scripts/follower.gd
@@ -3,21 +3,16 @@ extends PathFollow2D
 
 # member variables here
 var count_offset = 0.0
-
-# El siguiente valor en Intel(0.25) produce el movimiento rapido
-# mientras que en AMD tengo que aumentar 20 veces su valor
-# para que empiece a moverse similar al movimiento en Intel
-var delta_offset = 5 #0.25 # para Intel
-#var delta_offset = 0.25*20 # para AMD
-
+var delta_offset = 5
 
 func _ready():
 	# Initialization here
-	#set_process(true)
 	set_fixed_process(true)
+	set_process(true)
+	set_process_unhandled_input(true)
+	set_process_unhandled_key_input(true)
 
 
-#func _process(delta):
 func _fixed_process(delta):
 	count_offset+=delta_offset
 	
